d15ee9283bce81459034008fc7aad67192daf12c
helgaloa <helgaloa@gmail.com>
2013-11-24 22:00:28 +0000

    undirfrétt
    byrjuð að búa til fyrstu fréttasíðuna, er í undirmöppunni undir
    frett1.php

---
+++ undirsidur/frett1.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="is">
+
+<head>
+	<meta charset="utf-8">
+		<link rel="stylesheet" href="helper/mobile.css">
+		<link rel="stylesheet" href="helper/mobile_small.css">
+		<link rel="stylesheet" href="helper/mobile_big.css">
+		<link rel="stylesheet" href="helper/slider_style.css">
+
+		<script type="text/javascript" src="helper/jquery-2.0.3.js"></script>
+		<script type="text/javascript" src="helper/slider.js"></script>
+	<title>Forritunar-Fréttir</title>
+</head>
+
+
+<body>
+
+<section id="top">
+	
+	<section class="logomynd">
+	<img src="myndir/logo.png" class="logo" alt="Lógó síðunar">
+	</section>
+	
+	<section class="logotext">
+	<nav>
+		<a class="lk" href="index.php">HEIM</a>
+		<a class="lk" href="https://en.wikipedia.org/wiki/Guns_N%27_Roses">KENNSLUMYNDBÖND</a>
+		<a class="lk" href="https://en.wikipedia.org/wiki/Guns_N%27_Roses">HAFÐU SAMBAND</a>
+		<a class="lk" href="https://en.wikipedia.org/wiki/Guns_N%27_Roses">LEIKUR</a>
+	</nav>
+	</section>
+</section>
+
+	<section id="altmynd">
+		<div class="inpic">
+		<p> Áhugaverðar fréttir úr heimi forritunar!</p>
+		</div>
+
+	</section>
+
+
+	
+<div id="main">
+
+	<section id="I">
+		<article class="frett1">
+			<h1>Hugbúnaðarráðstefnan 2013</h1>
+			
+			<p class="frett">Hugbúnaðarráðstefna Ský
+					miðvikudaginn 20. nóvember á Grand hóteli kl. 13 - 16:30
+
+					"Straumar og stefnur í forritun"
+
+					Twitter: @SkyIceland #HugbRadst
+
+
+					Margt áhugavert er í gangi í heimi hugbúnaðargerðar og höfum við fengið nokkra af helstu sérfræðingum okkar til segja frá helstu áherslum: „Development – Social – Software Ecosystem – Big Data“
+
+					Taktu daginn strax frá og skráðu þig sem fyrst. Færð ekki betra tækifæri í bráð til að sjá og hitta vini þína í hugbúnaðargeiranum.
+
+					Dagskrá:
+
+					13:00-13:20  Frá harðvíruðu yfir í atburðadrifið (SOA2) - féll því miður niður vegna veikinda
+					                            Axel Valdemar Gunnlaugsson, 365
+
+					13:20-13:40  Samvinna nemenda á netinu (Peer promotions)
+					                            Björn Leví Gunnarsson, Brandeis University USA
+
+					13:40-14:00  Force.com og Platform-as-a-Service forritun
+					                             Ragnar Fjölnisson, Kaptio
+
+					14:00-14:20  Þjónustumiðuð kerfishögun með Node.js
+					                             Eiríkur Nilson, OZ
+
+					14:20-14:40  Kaffihlé - tengslanetið eflt
+
+					14:40-15:00  Greining gagna á flugi (Activity Stream)
+					                            Stefán Baxter, Flaumur
+
+					15:00-15:20  Kóðað í beinni (Play! 2.2)
+					                             Reynir Hubner, Hugsmiðjan
+
+					15:20-15:30   Örstutt pása - teygja úr sér og bæta á kaffibollann
+
+					15:30-15:50   Nýjar auðkenningarleiðir (RSA AA)
+					                            Peter Short, Landsbankinn
+
+					15:50-16:10   Frumgerðir með Meteor
+					                            Bjarni Ingimar Júlíusson, GreenQloud       
+
+					16:10-16:30   Gagnasöfnun - reynslusaga
+					                            Páll Hilmarsson, Datamarket
+
+					16:30               Ráðstefnuslit
+
+					Fundarstjóri:  Sigurður E. Guttormsson, Trackwell
+
+					Undirbúningsnefnd: Stjórn faghóps Ský um hugbúnaðargerð
+
+					Verð fyrir félagsmenn Ský: 9.500 kr.
+					Verð fyrir utanfélagsmenn: 14.500 kr.
+					Verð fyrir aðila utan vinnumarkaðar: 4.000 kr. </p>
+		
+			
+		</article>
+	</div>
+
+
+		
+
+<section class="bottom">
+	<section id="b1">
+	<aside class="b2">
+		<h2>Forritunar Fréttir</h2>
+		<ul>
+			<li><a class="bt" href="http://www.hi.is">Heim</a>
+			<li><a class="bt" href="http://www.hi.is">Kennslumyndbönd</a>		
+			<li><a class="bt" href="http://www.hi.is">Hafðu Samband</a>		
+			<li><a class="bt" href="http://www.hi.is">Leikur</a>			
+		</ul>
+	</aside>
+	
+	<aside class="b3">
+	<h2>Contact info:</h2>
+		<ul>
+			<li>ForritunarFréttir
+			<li>Laugavegi 38 - 101 Reykjavík		
+			<li>510 1212		
+			<li>forritunarfrettir@snilld.is		
+		</ul>	
+	</aside>
+	</section>
+
+
+	<footer>© Páll, Helga Lóa & Garðar 1977 - 2013</footer>
+</section>
+
+</body>
+
+</html>	
