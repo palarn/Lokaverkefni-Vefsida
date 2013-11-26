6492c26730ade8b30229e98db1b6503e077db5b4
gardard <gardard@hive.is>
2013-11-26 22:56:52 +0000

    nýtt kennslumyndband

--- undirsidur/video.php
+++ undirsidur/video.php
@@ -46,8 +46,13 @@
 		<article class="frett">
 			<h1>Mark Zuckerberg, stofnandi facebook, um forritunar kennslu</h1>
 
-		<p><iframe width="560" height="315" src="//www.youtube.com/embed/dmM_xDzy2nU" frameborder="0" allowfullscreen></iframe></p>
-	</article>
+			<p><iframe width="560" height="315" src="//www.youtube.com/embed/dmM_xDzy2nU" frameborder="0" allowfullscreen></iframe></p>
+		</article>
+		<article class="frett">
+			<h1>Kennslumyndbönd frá "thenewboston" sem er snillingur, hér er java kennslumyndbönd</h1>
+
+			<p><iframe width="560" height="315" src="//www.youtube.com/embed/Hl-zzrqQoSE?list=PL17E300C92CE0261A" frameborder="0" allowfullscreen></iframe></p>
+		</article>
 	</div>
 
 <h2>Komments</h2>
