5f73d5afa2e6710bbe0e32c5478dd64709e873e4
Páll Arnar <palarn@gmail.com>
2013-11-26 15:16:23 +0000

    leikur í slider

--- helper/slider.php
+++ helper/slider.php
@@ -5,7 +5,7 @@
 				array("Hugbúnaðarráðstefnan 2013","undirsidur/frett1.php","myndir/frett1.jpg"),
 				array("Er erfitt að læra forritun?","undirsidur/frett2.php","myndir/frett2.png"),
 				array("Stóra pöndumálið","undirsidur/panda.php","myndir/panda.png"),
-				array("Kisa 4","#","myndir/291.jpg"),
+				array("Hefur þú prófað leikinn okkar?","leikur/ASTEROIDS!.htm","myndir/leikur.png"),
 
 			);
 
