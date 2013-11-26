cbb397718a6a74d2f112553a64a38e399c9ff60b
helgaloa <helgaloa@gmail.com>
2013-11-26 15:13:46 +0000

    frétt 4 in the making

---
+++ undirsidur/frett4.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="is">
+
+<head>
+	<meta charset="utf-8">
+		<link rel="stylesheet" href="../helper/mobile.css">
+		<link rel="stylesheet" href="../helper/mobile_small.css">
+		<link rel="stylesheet" href="../helper/mobile_big.css">
+		<link rel="stylesheet" type="text/css" href="EasyPageComments.css"/>
+
+		<script type="text/javascript" src="../helper/jquery-2.0.3.js"></script>
+		<script type="text/javascript" src="EasyPageComments.js"></script>
+   		<script type="text/javascript" src="pushComments.js"></script>
+	<title>Forritunar-Fréttir</title>
+</head>
+
+
+<body>
+
+<section id="top">
+	
+	<section class="logomynd">
+	<img src="../myndir/logo.png" class="logo" alt="Lógó síðunar">
+	</section>
+	
+	<section class="logotext">
+	<nav>
+		<a class="lk" href="../index.php">HEIM</a>
+		<a class="lk" href="https://en.wikipedia.org/wiki/Guns_N%27_Roses">KENNSLUMYNDBÖND</a>
+		<a class="lk" href="https://en.wikipedia.org/wiki/Guns_N%27_Roses">HAFÐU SAMBAND</a>
+		<a class="lk" href="https://en.wikipedia.org/wiki/Guns_N%27_Roses">LEIKUR</a>
+	</nav>
+	</section>
+</section>
+
+	<section id="altmynd">
+		<div class="inpic">
+		<p></p>
+		</div>
+
+
+	</section>
+
+
+	
+<div id="main">
+
+	<section id="I">
+		<article class="frett1">
+
+			<p> ForritunarFréttir | 19.1.2013 | 07:52 </p>
+			<h1>Forritun, stelpur og grunnskólinn</h1>
+
+			<img src="../myndir/frett4.png" alt="Frétt3" align="left"><div class="frett"> Samkvæmt aðalnámskrá grunnskólanna eiga nemendur í 10. bekk að geta „nýtt hugbúnað við forritun og miðlun þekkingar á fjölbreyttan og skapandi hátt.“ Til þess að ná þessu markmiði fá þeir 80 mínútur á viku í upplýsinga- og tæknimenntun. Þá fá allar aðrar námsgreinirnar yfir 340 mínútur hver á viku. Þannig fær tæknimenntunin 2,68% af vikulegum kennslutíma. Aðalnámskráin lýsir upplýsinga- og tæknimenntuninni svo: „Undir þetta heyrir m.a. miðlamennt, skólasafnsfræði, tölvunotkun og upplýsinga- og samskiptatækni. [1] Hér er ekki minnst á forritun en samt sem áður eiga nemendur við lok 10. bekkjar að geta „nýtt hugbúnað við forritun“. Í aðalnámskránni er þar að auki sér kafli um upplýsinga- og tæknimennt. Í þeim kafla er ekki minnst á forritunarkennslu sem slíka heldur einungis þá staðreynd að nemandi í 10. bekk eigi að geta forritað. Hvernig hann á að læra það er hulin ráðgáta. Markmið námskránnar eru skýr en hvernig á að ná þeim er heldur erfiðara að sjá. </br></br>
+			Nú er ég 21 árs kvenkyns háskólanemi. Þegar ég hóf nám við heilbrigðisverkfræði í fyrra við Háskólann í Reykjavík þá man ég að á nýnemakynningunni var öllum deildunum skipt upp í stofur. Þegar tölvunarfræðideildin var kölluð þá hugsaði ég: „þarna fara allir lúðarnir“. Ég veit að ég var ekki eina stelpan í heiminum með þetta viðhorf. Á þessum tímapunkti kunni ég ekki að forrita og vissi í raun og veru ekkert hvað það snerist um. Í heilbrigðisverkfræðinni fór mér fljótt að leiðast, efnið átti ekki við mig, en sá áfangi sem náði athygli minni var Hagnýt forritun. Í þessum áfanga lærði ég á forritið Matlab. Mér til mikillar gleði uppgötvaði ég nýjan heim. Hér gat ég fengið tölvuna til að skapa það sem ég hugsaði með því að senda henni skipanir. Rökaðgerðir var eitthvað sem ég kynntist aðeins í stærðfræði í Menntaskólanum í Reykjavík en ekkert í líkingu við það að læra forritun. Það að læra að forrita breytti hugsun minni algjörlega. Mér fannst þetta æðislegt. Um jólin skipti ég yfir í hugbúnaðarverkfræði og nú svíf ég um á bleiku skýi inn í heim „lúðanna“.</br></br>
+			Ég er einstaklega heppin að hafa áttað mig svona fljótt í háskóla hvað ég vildi gera eða hvað? Þarna var heimur vísinda sem ég hafði aldrei kynnst áður. Ég viðurkenni það að þegar ég var í Menntaskólanum í Reykjavík stóð mér til boða að taka valáfanga í Java forritun. Ástæðan fyrir því að mér datt ekki til hugar að velja þann áfanga var sú að viðhorf mitt var mjög neikvætt vegna vanþekkingar. Þar að auki hafði ég ekki hugmynd um hvað forritun væri. Þarna tel ég grunnskólann hafa brugðist mér og ég tel hann enn í dag vera að bregðast forriturum framtíðarinnar því 80 klukkustundir á viku er ekki nóg, 2,68% er of lítið.</br></br>
+			Skólarnir þurfa að taka sér tak og tryggja það að börnin fái að kynnast möguleikum þeirra í framtíðinni. Þessar tölur hljóma fáránlega í mínum eyrum þar sem tæknin er allt í kringum okkur og tæknimenntað fólk verður sífellt eftirsóttara. Setja þarf skýra stefnu um forritun í grunnskólum. Þetta fag þarf að innleiða í grunnskólanna og það hefði átt að gerast fyrir mörgum árum.</br></br>
+			Í grunnámi í hugbúnaðarverkfræði í Háskólanum er Reykjavík hlutfall kvenna 11% og í tölvunarfræði er það 18%. Ein af ástæðunum fyrir þessu lága hlutfalli kvenna er vanþekking. Þar kemur grunnskólinn inn enn og aftur. Þessu þarf að breyta og grunnskólinn er í afar sterkri stöðu til þess að fræða krakkana um heim tækninnar. Til þess að þetta geti orðið að veruleika þarf að byrja á kennurunum. Nemi í kennslufræði við Háskóla Íslands getur valið sér upplýsingatækni og miðlun sem kjörsvið. Ef skoðuð eru námskeiðin sem neminn þarf að taka til þess að verða grunnskólakennari með upplýsingatæknimenntun kemur í ljós að forritun er ekki einu sinni í boði. [2]</br></br>
+			Því spyr ég aftur, hvernig getum við ætlast til þess að nemandi í 10. bekk læri að forrita þegar kennarinn hefur ekki fengið tækifæri til þess sjálfur í sínu námi. Ábyrgðin er því á herðum kennaranámsins sem og grunnskólanna og við þurfum að taka til hendinni ef við ætlum að dansa í takt við tæknisamfélag nútímans.</br>
+			</br>
+			<b>Höfundur:</b> Ingibjörg Ósk Jónsdóttir, nemandi í hugbúnaðarverkfræði við tölvunarfræðideild Háskólans í Reykjavík
+
+			</div>
+		
+			
+		</article>
+	</div>
+
+<h2>Komments</h2>
+
+    <div id="Comments"></div>
+
+    <h2>Skildu eftir komment</h2>
+
+    <div id="CommentForm"></div>
+		
+
+<section class="bottom">
+	<section id="b1">
+	<aside class="b2">
+		<h2>Forritunar Fréttir</h2>
+		<ul>
+			<li><a class="bt" href="http://www.hi.is">Heim</a>
+			<li><a class="bt" href="http://www.hi.is">Kennslumyndbönd</a>		
+			<li><a class="bt" href="http://www.hi.is">Hafðu Samband</a>		
+			<li><a class="bt" href="http://www.hi.is">Leikur</a>			
+		</ul>
+	</aside>
+	
+	<aside class="b3">
+	<h2>Contact info:</h2>
+		<ul>
+			<li>ForritunarFréttir
+			<li>Laugavegi 38 - 101 Reykjavík		
+			<li>510 1212		
+			<li>forritunarfrettir@snilld.is		
+		</ul>	
+	</aside>
+	</section>
+
+
+	<footer>© Páll, Helga Lóa & Garðar 1977 - 2013</footer>
+</section>
+
+</body>
+
+</html>	
