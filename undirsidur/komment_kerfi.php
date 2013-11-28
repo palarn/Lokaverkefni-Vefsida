f50e9dd16e7ddc8796059b8bd3ddba301e396d06
Páll Arnar <palarn@gmail.com>
2013-11-28 15:23:02 +0000

    lgaði komment

--- undirsidur/komment_kerfi.php
+++ undirsidur/komment_kerfi.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="is">
-
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="EasyPageComments.css"/>
-
-		<script type="text/javascript" src="../helper/jquery-2.0.3.js"></script>
-		<script type="text/javascript" src="EasyPageComments.js"></script>
-   		<script type="text/javascript" src="pushComments.js"></script>
-   	</head>
-<body>
-
 <h2>Komments</h2>
 
     <div id="Comments"></div>
@@ -18,5 +5,4 @@
     <h2>Skildu eftir komment</h2>
 
     <div id="CommentForm"></div>
-  </body>
-  </html>
+ 
